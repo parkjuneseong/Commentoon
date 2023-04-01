3d616433e54ff65b29efe8e3e7f6155462a86e34
june <eptm917@gmail.com>
2023-04-01 13:42:27 +0900

    test commit

---
+++ Commentoon/Commentoon/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  IOS-Practice2
+//
+//  Created by June on 2022/11/01.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController =  UINavigationController(rootViewController: MainViewController())
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
---
+++ Commentoon/Commentoon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Commentoon
+//
+//  Created by 박준성 on 2023/04/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
